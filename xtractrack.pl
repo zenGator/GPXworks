0e0bc6add26d32678673be570699b98f482a1ddf
zG <22377978+zenGator@users.noreply.github.com>
2024-05-06 10:50:21 -0400

    add auto-select:  route or track (<rte>|<trk>)

--- xtractrack.pl
+++ xtractrack.pl
@@ -2,9 +2,8 @@
 # https://github.com/zenGator/xtractrack.pl
 # zG:20240505
 
-# other notes
-# pull track names from RayMarine-generated .gpx
-# ToDo:  give option to pull track OR route names
+#other notes:
+#	pull track or route names from RayMarine-generated .gpx
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #use and constants here
@@ -14,17 +13,28 @@ use warnings;
 use Getopt::Std;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#declare variables here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+my $x=0;  #primary counter, the line we are currently processing
+my $name;	#route or track name currently being processed
+my $trackCount=0;	#how many have we found?
+my $commandname=$0=~ s/^.*\///r;
+my %opt=();
+my ($target, $type); #route or track
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #declare subroutines here
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sub usage;
-#sub capOutput;
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst,$time);
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#pre-processing (options)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # switches followed by a : expect an argument
 # see usage() below for explanation of switches
-my $commandname=$0=~ s/^.*\///r;
-my %opt=();
-getopts('hi:o:', \%opt) or usage();
-
+getopts('hi:o:rt', \%opt) or usage();
 
 usage () if ( $opt{h} or (scalar keys %opt) == 0 ) ;
 
@@ -38,37 +48,51 @@ if ($opt{o}) {
         or die "Could not open file '$opt{o}': $!\n";
     }
 
+if ($opt{r}) {
+	$target="<rte>" ;
+	$type="route";
+	}
+if ($opt{t}) {
+	$target="<trk>" ;
+	$type="track";
+	}
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#declare variables here
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
-my $x=0;  #primary counter, the line we are currently processing
-my $trackCount=0;
-my $outFiCount=0;
-my $name;	#track name currently being processed
-
-
-main: while (my $row = <$fh>) {
-    #get a line
+#main program block
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+main: while (my $row = <$fh>) {    #get a line
     $x++;  # increment line counter
     chomp $row;  #drop the newline at the end of the row
-    my $orig_row=$row;	#store this off for later reference
-	if ($row =~/<trk>/) {  # we're looking for the various tracks
+	while (! defined $target) {	# default behaviour:  no -[r|t] specified
+		if ($row =~ /<rte>|<trk>/){	# maybe this line begins a track/route block
+			($target = $row) =~ s/^\s*(.*)\s/$1/;	#if so, set target, use the special 'r' modifier
+			$type="route" if $target =~/<rte>/;		#also need to identify type
+			$type="track" if $target =~/<trk>/;
+			printf "\nINFO: type of extract not specified.  First thing found is a %s, so that's what we'll report on.\n\n", $type; # notify
+			}
+		else {		#if not, grab another line as we normally would
+			$row=<$fh>;
+			$x++;
+			chomp $row;
+			}
+		}
+	if ($row =~/$target/) {  # we're looking for either tracks or routes
 		$trackCount++;
-		$row=<$fh>;	# the <trk> tag signals a new block of tracks, and the next item should be <name>
+		$row=<$fh>;	# the [$target] tag signals a new block, and the next item should be <name>
 		$x++;  # increment line counter
 		chomp $row;  #drop the newline at the end of the row
-		if ($row !~/^\s*<name>/){
-			die "expected a nametag at $x: $row\n";
+		if ($row !~/^\s*<name>/){	#after the rte or trk tag, there should be a <name> tag
+			die "expected a nametag at $x: $row\n";	#if not, abort
 			}
 		$name=$row;
-		$name=~s/^\s*<name>(.*)<\/name>/$1/;
-		print "track #$trackCount: $name\n";
+		$name=~s/^\s*<name>(.*)<\/name>/$1/;	#strip the nametag
+		printf $outFH "%s #%d: %s\r\n", $type,$trackCount,$name;	#output
 		}
 	}
 
 sub usage() {
-    print "like this: \n\t".$commandname." -i [infile] -o [outfile]\n";
+    print "like this: \n\t".$commandname." -i [infile] -o [outfile] -[t|r]\n";
     print "\nThis pulls the names of tracks from a RayMarine .gpx file.\n";
+	print "\nuse -t for tracks, and -r for routes.  If neither is specified, the first type (track or route) encountered will be used.\n";
     exit 1;
     }
