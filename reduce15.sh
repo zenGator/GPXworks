3e54623ff8b1fb198ff18e7009b5563778f2b236
zG <22377978+zenGator@users.noreply.github.com>
2024-07-07 07:26:21 -0400

    Create reduce15.sh

---
+++ reduce15.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# zG:20240707
+# data reduction:  only 1 waypoint per 15 seconds
+
+awk 'BEGIN{oldtime=0}; 
+	match($0,/<time>([-0-9:TZ]{20})<\/time>/,m) {
+		rawtime=m[1];
+		split(rawtime,daytime,"T"); 
+		split(daytime[1],date,"-"); 
+		daytime[2]=substr(daytime[2],1,8); 
+		split(daytime[2],time,":"); 
+		newtime=mktime(sprintf("%s %s %s %s %s %s",date[1],date[2],date[3],time[1],time[2],time[3])); 
+		delta=newtime-oldtime; 
+		if(delta>15){
+			print $0;
+			oldtime=newtime;
+			}
+		next;
+	}{print $0};' "$1"
